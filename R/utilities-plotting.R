894719e7ac2ee4de1180752c3d78d793e0124309
Matt Harris <m.harris@gns.cri.nz>
2025-03-25 12:09:56 +1300

    add verbose pass to pre cpt plotting

--- R/utilities-plotting.R
+++ R/utilities-plotting.R
@@ -121,7 +121,8 @@ plot_cpt_pre <- function(PrC_results,
                          age_upperbound,
                          age_lowerbound,
                          rev_y,
-                         uncertainty_res){
+                         uncertainty_res,
+                         verbose = verbose){
   # Get PrC results
   # PrC_results <- generate_PrC(site_data = site_data,
   #                             age_upper = age_upper,
